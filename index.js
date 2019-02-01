c031bb0e06868055db81e5512d01d76b92045a92
Oli Nelson <olivernelson@icloud.com>
2019-02-01 16:39:45 -0500

    limited results working

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 let allMonsters = []
-
+let monsterContainer = {}
 document.addEventListener("DOMContentLoaded", function(){
   console.log("Hi Gabbbie not Gabby");
-  const monsterContainer = document.querySelector('#monster-container')
+    monsterContainer = document.querySelector('#monster-container')
 
   fetch('http://localhost:3000/monsters')
   .then(function(response){
   return response.json()
 }).then(function(response){
   allMonsters = response
+  firstFiftyMonsters = allMonsters.slice(0,50)
+  showMonstersSetLength(firstFiftyMonsters)
   // displays all monsters function goes here...
 })
 
 
+}) //end of DOMContentLoaded
 
 
-})
-
-
-function showMonstersSetLength(allMonsters) {
-
-  for(let monster of allMonsters){
+function showMonstersSetLength(monsters) {
+  let i = 1
+  monsterContainer.innerHTML = ''
+  for(let monster of monsters){
     monsterContainer.innerHTML +=
-    `<div "${}">
-    <h4>${}</h4>
+    `<div data-id="${monster.id}">
+    <p>${i}</p>
+    <h4>${monster.name}</h4>
+    <p>${monster.age}</p>
+    <p>${monster.description}</p>
     </div>
     `
+    i++
   }
 
 }
